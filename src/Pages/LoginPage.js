f56808f9af56dc611955d9a9185e513011052521
lucas <lucas.landru@student.junia.com>
2021-11-12 16:42:28 +0100

    login css not finish

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,12 +1,17 @@
-import 'CSS/loginPage.css'
+import "CSS/loginPage.css";
 
-export default function LoginPage({location}){
+export default function LoginPage({ location }) {
+  console.log(location);
 
-    console.log(location)
-
-    return(
-       <div class="body">
-           Login
-       </div> 
-    )
+  return (
+    <div class="body">
+      <div class="form">
+        {/* <image /> */}
+        {/* <div class="inputs-wrapper">
+          <input>Username</input>
+          <input>Password</input>
+        </div> */}
+      </div>
+    </div>
+  );
 }
