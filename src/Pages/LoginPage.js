14494d3b6a1a1ddba08bf38d917a9f5378bde2e8
lucas <lucas.landru@student.junia.com>
2021-11-15 11:24:07 +0100

    login css done

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,3 +1,4 @@
+import "CSS/colors.css";
 import "CSS/loginPage.css";
 
 export default function LoginPage({ location }) {
@@ -5,17 +6,30 @@ export default function LoginPage({ location }) {
 
   return (
     <>
-      <div class="container-fluid vh-100">
+      <div class="container-fluid vh-100 grey">
         <div class="container h-100 py-5">
           <div class="row h-100 justify-content-center d-flex">
             <div class="col-12 col-md-9 col-lg-8 col-xl-7">
-              <div class="card bg-dark text-white h-100">
+              <div class="card h-100">
                 <div class="card-body p-5 text-center">
                   <div class="">
-                    <h3 class="mb-3">Login</h3>
+                    <img class="card-img-top" src="/Logo_Gusto_Colors.svg" alt="Card image cap"/>
                     <div class="input-group mb-3">
                       <span class="input-group-text" id="basic-addon1">
-                        @
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-person-circle"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                          <path
+                            fill-rule="evenodd"
+                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                          />
+                        </svg>
                       </span>
                       <input
                         type="text"
@@ -26,30 +40,39 @@ export default function LoginPage({ location }) {
                       />
                     </div>
                     <div class="input-group mb-3">
-                      <span class="input-group-text" id="basic-addon1">
-                        @
+                      <span class="input-group-text" id="basic-addon2">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-lock-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
+                        </svg>
                       </span>
                       <input
                         type="password"
                         class="form-control"
                         placeholder="Password"
                         aria-label="Password"
-                        aria-describedby="basic-addon1"
+                        aria-describedby="basic-addon2"
                       />
                     </div>
                     <div class="form-check d-flex mb-2">
                       <input
-                        class="form-check-input"
+                        class="form-check-input blue-checked"
                         type="checkbox"
                         value=""
                         id="flexCheckChecked"
-                        checked
+                        // checked
                       />
                       <label class="form-check-label" for="flexCheckChecked">
-                      &nbsp; Restez connectez ?
+                        &nbsp; Restez connectez ?
                       </label>
                     </div>
-                    <button type="submit" class="btn btn-primary mb-2 col-12">
+                    <button type="submit" class="btn btn-dark blue mb-2 col-12">
                       Connexion
                     </button>
                   </div>
