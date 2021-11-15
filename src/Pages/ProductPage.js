7b46bd16d74cc7035fbe4c8171e9b604b19349bd
Nicolas <nicolas.defoort@isen.yncrea.fr>
2021-11-15 14:59:29 +0100

    update productPage

--- src/Pages/ProductPage.js
+++ src/Pages/ProductPage.js
@@ -8,17 +8,6 @@ import { useState } from "react"
 
 
 export default function ProductsPage(){
-    const[check,setchecked]=useState(false)
-
-    function handleCheck(){
-        if (check==true){
-            setchecked(false)
-        }
-        else{
-            setchecked(true)
-        }
-        
-    }
 
     return(
         <div className="products-container">
@@ -27,27 +16,13 @@ export default function ProductsPage(){
                     Ajouter un produit 
                 </div>
                 <div className="addProduct-container-form">
+                    <div className="addProduct-container-form-categories">
+                        <SelectForm className="" one="Fruit" two="Boisson" three="dessert"/>
+                    </div> 
                     <InputForm label="Prix" type="number"/>
                     <InputForm label="Nom du produit" type="text"/>
                     <UploadForm/>
-                    <div className="addProduct-container-form-categories">
-                        <SelectForm className="" one="Fruit" two="Boisson" three="dessert"/>
-                        <div className="checkboxForm-container">
-                            <input className="checkboxForm-container-input" type="checkbox" id=""/>
-                            <label className="checkboxForm-container-label" for="gridCheck">
-                                <div className="d-flex flex-column">
-                                    <div>Nouvelle</div>
-                                    <div>catégorie</div>
-                                </div>
-                            </label>
-                        </div>
-                    </div> 
-                    {check?<div>a</div>:false}
-                    
-                   
-                </div> 
-                
-                
+                </div>
             </div>
         </div>
     )
