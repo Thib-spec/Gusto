b1170c87cd6d573487479f6c7c851e2656bea1b6
Nicolas <nicolas.defoort@isen.yncrea.fr>
2021-11-17 09:14:19 +0100

    update categories.js

--- src/Pages/CategoriesPage.js
+++ src/Pages/CategoriesPage.js
@@ -3,6 +3,8 @@ import { useState } from "react"
 import DATACatégories from "../Data/categories"
 import DATAProducts from "../Data/products"
 import dot from "Images/dots.png"
+import InputForm from "Components/FormComponent/InputForm"
+import imgcategorie from "Images/imgcategorie.svg"
 
 
 export default function CategoriesPage(){
@@ -25,24 +27,38 @@ export default function CategoriesPage(){
                 <div className="categories-list-element">
                     <div className="categories-list-element-title">
                         <div>{categories.name}</div>
-                        <div className="categories-dot"><img width="100%" src={dot} alt=""/></div>
+                        <div className="categories-dot" onClick={handleClick}><img width="100%" src={dot} alt=""/></div>
+                       
                     </div>
-
-                    <div className="categories-list-element-sub">
-                            <div className="categories-list-element-sub-description">{categories.description}</div>
-                        {/* {DATAProducts.map((product)=>(
-                            <div className="categories-list-element-sub-element">
-                                {product.categorie===categories.id?<div>{product.name}</div>:false}<div></div>
+                    {
+                        click?
+                            
+                            <div className="categories-list-element-sub">
+                                <div className="categories-list-element-sub-description">
+                                    <img src={imgcategorie} alt=""/>
+                                    <textarea type="text" className=" categories-list-element-sub-description-input"  placeholder="Description de la catégorie" value={categories.description}/>
+                                    
+                                </div>
                             </div>
-                        ))} */}
-                    </div>
+                            
+                            
+                        :false
+                        } 
+
+                    
                     
                     
                 </div>
                 ))}
             </div>
-
-            
+        {/* {DATAProducts.map((product)=>(
+                            <div className="categories-list-element-sub-element">
+                                {product.categorie===categories.id?<div>{product.name}</div>:false}<div></div>
+                            </div>
+                        ))} */}  
+                        
+        {/* <div className="categories-dot-menu-element">Modifier</div>
+                                <div className="categories-dot-menu-element">Supprimer</div>  */}
             
         </div>
     // <div className="">
@@ -51,3 +67,4 @@ export default function CategoriesPage(){
     // </div>
     )
 }   
+
