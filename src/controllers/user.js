a02457ef52fe91ab0e506e1a6c6d2cf4b18ec26b
Thib <thib.blasselle@gmail.com>
2021-11-17 10:20:52 +0100

    Route user getById

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,19 +4,22 @@ const Model = require("../database/models");
 // }
 
 
-
-const {DataTypes,sequelize } = require("sequelize");
-
-
     exports.listUsers = (req, res) => {
         Model.Users.findAll()
         .then(user => res.status(200).json(user))
         .catch(error => res.status(400).json(error))
-        // return users
-        //     .then((users) => res.status(200).json(users))
-        //     .catch((error) => {
-        //         console.log(error);
-        //         return res.status(500).json({ message: 'Internal error' });
-        //     });
-        // res.send("hello")
+    }
+
+
+    exports.getUserById = (req,res) => {
+        console.log("ok")
+        Model.Users.findOne({
+            where:{
+                Id_user : req.params.id
+            }
+        })
+        .then(user => res.status(200).json(user))
+        .catch(error => res.status(400).json(error))
+     
+        
     }
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,8 @@
 const users = require('express').Router();
 const userController = require("../controllers/user")
 
+users.get('/user/:id',userController.getUserById)
 users.get('/users', userController.listUsers)
+
 
 module.exports = users
