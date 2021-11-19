fd5cfd6a8f34acc93a81daafc5907178a5609461
Thib <thib.blasselle@gmail.com>
2021-11-19 10:12:21 +0100

    Add commons controller for category

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -72,3 +72,82 @@ exports.addCategory = (req,res) =>{
 
         
 }
+
+
+exports.editCategory =(req,res) => {
+
+    const { label, image, description} = req.body
+
+    Model.Categories.findOne({
+        where: {
+            id_category: req.params.id
+        }
+    })
+
+    .then((category) => {
+        if (!category) {
+            return res.status(400).json({
+                message: 'Category does not exist',
+            });
+        }
+
+        const editCategorySchema = Joi.object().keys({ 
+            label: Joi.string(),
+            image: Joi.string(),
+            description: Joi.string().email(), 
+        })
+
+        const result = editCategorySchema.validate(req.body)
+
+        const {error } = result; 
+        const valid = error == null; 
+        if (!valid) { 
+          res.status(400).json({ 
+            message: 'One or more fields are not well written', 
+          }) 
+        } 
+        
+        else { 
+            Model.Categories.update({
+                label: label,
+                image: image,
+                description: description,
+            },
+            {
+                where : {
+                    id_category: req.params.id
+                }
+            })
+            .then(res.send("Modification apply"))
+        }
+    })
+    
+    .catch(error => console.log(error))
+
+}
+
+
+exports.deleteCategory = (req,res) => {
+    
+    Model.Categories.findOne({
+        where: {
+            id_category: req.params.id
+        }
+    })
+    .then((category) => {
+        if (!category) {
+            return res.status(400).json({
+                message: 'Category does not exist',
+            });
+        }
+    Model.Categories
+            .destroy({
+                where: {
+                    id_category: req.params.id
+                }
+            }).then(res.send(`Category with id : ${req.params.id} has been deleted`))
+        }
+
+    )
+    .catch(error => res.status(400).json(error))
+}
--- src/routes/category.js
+++ src/routes/category.js
@@ -5,8 +5,8 @@ const passport = require("passport")
 categories.get('/',categoryController.listCategories)
 categories.get('/:id', categoryController.getCategoryById)
 categories.post('/',categoryController.addCategory)
-// category.put('/category/:id',categoryController.editcategory)
-// category.delete('/category/:id',categoryController.deletecategory)
+categories.put('/:id',categoryController.editCategory)
+categories.delete('/:id',categoryController.deleteCategory)
 
 
 module.exports = categories
