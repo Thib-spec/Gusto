deb7a85a351699af7462b9e65891ee2a614216a4
Thib <thib.blasselle@gmail.com>
2021-12-02 12:16:24 +0100

    Common controller for FridgePreset

--- src/controllers/fridgePreset.js
+++ src/controllers/fridgePreset.js
@@ -2,6 +2,153 @@ const Model = require("../database/models");
 const Joi = require('joi');
 const { Op } = require("sequelize");
 
+exports.listFridgePreset = (req, res) => {
+    Model.FridgePresets.findAll()
+    .then(fridgePreset => res.status(200).json(fridgePreset))
+    .catch(error => res.status(400).json(error))
+}
+
+
+exports.getFridegPresetById = (req,res) => {
+    Model.FridgePresets.findOne({
+        where:{
+            id_fridgePresets : req.params.id
+        }
+    })
+    .then((fridgePreset) => {
+        if (!fridgePreset) {
+            return res.status(400).json({
+                message: 'FridgePreset not found',
+            });
+        }
+
+        else {
+            return res.status(200).json(fridgePreset)
+        }
+    })
+    .catch(error => res.status(400).json(error))
+ 
+    
+}
+
+
+
+exports.addFridgePreset = (req,res) =>{
+    const {label,fk_id_client} = req.body;
+
+
+   const postFridgePresetSchema = Joi.object().keys({ 
+    label: Joi.string().required(),
+    fk_id_client: Joi.number().required()
+    })
+
+    const result = postFridgePresetSchema.validate(req.body)
+
+    const {error } = result;
+
+    const valid = error == null;
+
+    if (!valid) {
+    res.status(400).json({ 
+        message: 'Missing required parameters',
+        info: 'Requires: label, fk_id_client' 
+    })
+    }
+
+
+
+    else {
+
+    Model.FridgePresets.create({
+        label: label,
+        fk_id_client:fk_id_client
+    })
+    
+    .then(fridgePreset => res.status(200).json(fridgePreset))
+    .catch(error => res.status(400).json(error))
+    }
+}
+
+
+exports.editFridgePreset = (req,res) => {
+    const {label,fk_id_client} = req.body;
+
+    Model.FridgePresets.findOne({
+        where: {
+            id_fridgePresets: req.params.id
+        }
+    })
+
+    .then((fridgePreset) => {
+        if (!fridgePreset) {
+            return res.status(400).json({
+                message: 'FridgePreset not found',
+            });
+        }
+
+        const editFridgePresetSchema = Joi.object().keys({ 
+            label: Joi.string(),
+            fk_id_client: Joi.number()
+        })
+
+        const result = editFridgePresetSchema.validate(req.body)
+
+        const {error } = result; 
+        const valid = error == null; 
+
+        if (!valid) { 
+          res.status(400).json({ 
+            message: 'One or more fields are not well written', 
+          }) 
+        } 
+        
+        else { 
+            Model.FridgePresets.update({
+                label: label,
+                fk_id_client:fk_id_client
+            },
+            {
+                where : {
+                    id_fridgePresets: req.params.id
+                }
+            })
+            return res.send("Modification apply")
+        }
+    })
+    
+    .catch(error => console.log(error))
+}
+
+
+exports.deleteFridgePreset = (req,res) => {
+    
+    Model.FridgePresets.findOne({
+        where: {
+            id_fridgePresets: req.params.id
+        }
+    })
+    .then((FridgePreset) => {
+        if (!FridgePreset) {
+            return res.status(400).json({
+                message: 'FridgePreset not found',
+            });
+        }
+    Model.FridgePresets
+            .destroy({
+                where: {
+                    id_fridgePresets: req.params.id
+                }
+            }).then(() => res.send(`FridgePreset with id : ${req.params.id} has been deleted`))
+        }
+
+    )
+    .catch(error => res.status(400).json(error))
+}
+        
+
+
+
+
 // ajout d'un produit dans un preset
 
 exports.addFrontProduct = (req,res) =>{
