ab95f68b6e7626d1923274db7305dc907647c4af
Nicolas <nicolas.defoort@isen.yncrea.fr>
2021-12-10 13:34:14 +0100

    update log fix errors

--- src/Components/FridgePage2/LogSaleComponent.js
+++ src/Components/FridgePage2/LogSaleComponent.js
@@ -2,22 +2,21 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function LogSaleComponent({ sale }) {
+
   console.log(sale);
+  let amount = sale.cbcless_amount+sale.cbemv_amount+sale.lv_quantity*sale.lv_amount
 
   return (
     <div className="logs-sales-content-element">
       <div className="logs-sales-date">{sale.sales_timestamp}</div>
       <div className="logs-sales-prix">
-        {sale.cbcless_amount}+{sale.cbemv_amount}
+        {amount}
       </div>
       <div className="logs-sales-content-element-products">
         {sale.Products.map((el) => (
-          <div
-            className="logs-sales-content-element-products-element"
-            key={el.id_product}
-          >
+          <div className="logs-sales-content-element-products-element" key={el.id_product}>
             <div>{el.label}</div>
-            <div>x{el.products_sales.quantity_product}</div>
+            <div> x{el.products_sales.quantity_product}</div>
           </div>
         ))}
       </div>
--- src/helpers/ArrayController/ArrayControllerMenu.js
+++ src/helpers/ArrayController/ArrayControllerMenu.js
@@ -1,78 +1,84 @@
+import Value from "helpers/Value";
+
 const copyy = (obj) => {
-    return JSON.parse(JSON.stringify(obj));
-  };
+  return JSON.parse(JSON.stringify(obj));
+};
 
+function Element(el, ArrayController) {
+  this._value = el;
+  this.value = ArrayController.findElById(el.id);
+  this.isInArray = this.value ? true : false;
+  this.ArrayController = ArrayController;
+}
 
-  function Element(id, ArrayController){
-    this.value = ArrayController.findElById(id)
-    this.ArrayController = ArrayController
-  }
-  
-  Element.prototype.update = function(body){
-    this.ArrayController.addOrUpdateMany([{...this.value, ...body}])
-  }
-  
-  Element.prototype.remove = function(){
-    this.ArrayController.removeMany([this.value])
-  }
+Element.prototype.update = function (body) {
+  this.ArrayController.addOrUpdateMany([{ ...this.value, ...body }]);
+  return this;
+};
 
-  export default function ArrayControllerMenu([value, setValue],[initValue, setInitValue]) {
-    this.value = value;
-    this.setValue = setValue;
-  
-    // this.initValue = copyy(value);
-    this.initValue = initValue;
-    this.setInitValue = setInitValue;
+Element.prototype.set = function (body) {
+  this.value = body;
+};
 
+Element.prototype.remove = function () {
+  this.ArrayController.removeMany([this.value]);
+};
 
+export default function ArrayController(valueUseState, initValueUseState) {
+  this._value = new Value(valueUseState);
+  this.initValue = new Value(initValueUseState);
+  this.value = this._value.value;
+}
 
-    ArrayControllerMenu.prototype.set = function (JSONArray, options) {
-        // const JSONArrayCopy = copyy(JSONArray);
-        if (options && options.init === true) this.setInitValue(copyy(JSONArray))
-        this.setValue(copyy(JSONArray));
-      };
+ArrayController.prototype.set = function (JSONArray, options) {
+  // const JSONArrayCopy = copyy(JSONArray);
+  if (options && options.init === true) this.initValue.set(copyy(JSONArray));
+  this._value.set(copyy(JSONArray));
+};
 
+ArrayController.prototype.get = function (el) {
+  return new Element(el, this);
+};
 
-    ArrayControllerMenu.prototype.findElById = function (id) {
-    return this.value.find((JSONelement) => id == JSONelement.id);
-    };
+ArrayController.prototype.clear = function () {
+  this.set([]);
+};
 
-    ArrayControllerMenu.prototype.get = function (id) {
-    return new Element(id, this)
-    };
+ArrayController.prototype.reset = function () {
+  this.set(copyy(this.initValue.value));
+};
 
+ArrayController.prototype.findIndexById = function (id) {
+  return this.value.findIndex((JSONelement) => id == JSONelement.id);
+};
 
-    ArrayControllerMenu.prototype.reset = function () {
-        this.set(this.initValue);
-    };
-    ArrayControllerMenu.prototype.clear = function () {
-        this.set([]);
-      };
+ArrayController.prototype.findElById = function (id) {
+  return this.value.find((JSONelement) => id == JSONelement.id);
+};
 
-      ArrayControllerMenu.prototype.addOrUpdateMany = function (JSONArray, options) {
-        let copy = copyy(this.value);
-        JSONArray.forEach((el) => {
-          const index = copy.findIndex((JSONelement) => el.id == JSONelement.id);
-          if (index > -1) copy[index] = el;
-          else copy = [...copy, el];
-        });
-        this.set(copy, options);
-      };
-      
-      ArrayControllerMenu.prototype.removeMany = function (JSONArray, options) {
-        let copy = copyy(this.value);
-        JSONArray.forEach((el) => {
-          const index = copy.findIndex((JSONelement) => el.id == JSONelement.id);
-          if (index > -1) copy.splice(index, 1);
-        });
-        this.set(copy, options);
-      };
+ArrayController.prototype.hasElById = function (id) {
+  return this.value.findIndex((JSONelement) => id == JSONelement.id) > -1;
+};
 
-   
+ArrayController.prototype.addOrUpdateMany = function (JSONArray, options) {
+  let copy = copyy(this.value);
+  JSONArray.forEach((el) => {
+    const index = copy.findIndex((JSONelement) => el.id == JSONelement.id);
+    if (index > -1) copy[index] = el;
+    else copy = [...copy, el];
+  });
+  this.set(copy, options);
+};
 
+ArrayController.prototype.removeMany = function (JSONArray, options) {
+  let copy = copyy(this.value);
+  JSONArray.forEach((el) => {
+    const index = copy.findIndex((JSONelement) => el.id == JSONelement.id);
+    if (index > -1) copy.splice(index, 1);
+  });
+  this.set(copy, options);
+};
 
-  }
-  
-
-  
- 
+ArrayController.prototype.json = function () {
+  return this.value;
+};
