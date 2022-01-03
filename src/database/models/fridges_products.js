b20523b35bbd92cd25cd937d6ef51e6730224ded
Thib <thib.blasselle@gmail.com>
2022-01-03 16:16:59 +0100

    fix typo

--- src/database/models/fridges_products.js
+++ src/database/models/fridges_products.js
@@ -7,6 +7,7 @@ module.exports = (sequelize) => {
     // Cependant , on avit besoin d'ajouter le champ "quantity" dans cette table, on a donc créé un model du même nom que la table de liaison afin de pouvoir la modifier
     // C'est pourquoi dans certain controllers, lorsque le modèle existe, on peut y accéder par Model.["ModelName"] mais sinon on est obligé de passer par les fonctions pré-faite de sequelize afin d'accéder aux données
     // A noter que "id_fridges_products" n'est jamais utilisé mais est nécessaire pour la bonne création du modèle
+    
     class fridges_products extends Model{
     }
 
